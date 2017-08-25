0198d1800cff70face496ecf667b4f0232a2f7f5
Tolga Kayhan <kayhantolga@hotmail.com>
2017-08-25 18:54:47 +0300

    Critical bug fix. nuget package metadata updated

--- FacebookAccountKit/FacebookAccountKitSdk.cs
+++ FacebookAccountKit/FacebookAccountKitSdk.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using FacebookAccountKit.ResponseModels;
 using TolgaTheWizard.ErrorHandler;
-using TolgaTheWizard;
+using TolgaTheWizard.Extensions;
 
 namespace FacebookAccountKit
 {
