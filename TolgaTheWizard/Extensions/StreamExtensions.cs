0f8fa6c05be860c447950addf11386eb2ec05d04
Tolga Kayhan <kayhantolga@hotmail.com>
2017-09-06 14:52:33 +0300

    Facebook Account Kit moved to another solution. MemoryStream extension class added.

---
+++ TolgaTheWizard/Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace TolgaTheWizard.Extensions
+{
+
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Convert string to stream
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static Stream ToStream(this string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
