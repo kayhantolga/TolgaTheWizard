786487303bd2dfbddeb2268bc8805fad43f889e1
Tolga Kayhan <kayhantolga@hotmail.com>
2017-09-29 15:45:09 +0300

    Location extensions added(ToDbGeography). Coordinate class added.

---
+++ TolgaTheWizard/Extensions/LocationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity.Spatial;
+
+namespace TolgaTheWizard.Extensions
+{
+    public static class LocationExtensions
+    {
+        /// <summary>
+        /// Converts given location to DbGeography
+        /// </summary>
+        /// <param name="longitude">longitude</param>
+        /// <param name="latitude">latitude</param>
+        /// <returns></returns>
+        private static DbGeography ToDbGeography(double longitude, double latitude)
+        {
+            return DbGeography.FromText($"POINT({longitude} {latitude})");
+        }
+
+        /// <summary>
+        /// Converts given location to DbGeography
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        private static DbGeography ToDbGeography(this Coordinate coordinate)
+        {
+            return DbGeography.FromText($"POINT({coordinate.Longitude} {coordinate.Latitude})");
+        }
+    }
+}
